da9690e0b1ba95f63114a55b9dd7a74a2bd22244
Asif Khan <asif.khan@northsouth.edu>
2016-10-15 01:20:11 +0600

    Add files via upload

---
+++ Bullet.java
@@ -0,0 +1,51 @@
+package bVp;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+public class Bullet{
+	
+	private int x,y;
+	private String imagePath;
+	
+	public Bullet(int x, int y, String imagePath) {
+		
+		
+		this.x = x;
+		this.y = y;
+		this.imagePath = imagePath;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public void draw(Graphics g){
+		
+		ImageIcon bullet = new ImageIcon(imagePath);
+		g.drawImage(bullet.getImage(),x,y,null);
+		
+	}
+	
+}
+
---
+++ GameSound.java
@@ -0,0 +1,58 @@
+package bVp;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class GameSound {
+
+	public static void bulletFiredSound(){
+
+		try{	
+			File sound = new File("sounds//BulletFired//bulletWhizzing.wav");
+
+			AudioInputStream ais = AudioSystem.getAudioInputStream(sound);
+			Clip clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+		}
+		catch(Exception e){
+
+		}
+	}
+
+
+	public static void bulletHitSound(){
+
+		try{	
+			File soundHit = new File("sounds//BulletFired//SilencerShot.wav");
+
+			AudioInputStream ais = AudioSystem.getAudioInputStream(soundHit);
+			Clip clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+		}
+		catch(Exception e){
+
+		}
+
+	}
+
+
+	public static void Shooter_Bee_Collide_Sound(){
+
+		try{	
+			File sound = new File("sounds//BulletFired//Blast.wav");
+
+			AudioInputStream ais = AudioSystem.getAudioInputStream(sound);
+			Clip clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+		}
+		catch(Exception e){
+
+		}
+	}
+
+}
